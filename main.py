0e693764db0c420ec3a7d65bb32822a3754b4773
Gravity <137218519+gravitacia@users.noreply.github.com>
2025-10-08 20:12:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from components.token import get_valid_token_results  # or whatever the function name is
-results = get_valid_token_results()  # call the function to get the data
+from components.token import get_valid_token_results
+results = get_valid_token_results()
 
 for token in results:
     print(token)
